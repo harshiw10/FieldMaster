2140ca13f4a94f1b4db60fa5f3841828f8914ed9
harshiw10 <harshiwije2001@gmail.com>
2024-06-04 11:53:00 +0530

    URL Changes to work with the machine

--- WebApp/client/src/components/Fence/Fence/fence.js
+++ WebApp/client/src/components/Fence/Fence/fence.js
@@ -115,7 +115,7 @@ export default function Fence({ onBackToSidebar }) {
 
       // Make POST request to the backend
       const response = await axios.post(
-        "http://192.168.52.237:3000/api/fence/fence",
+        "http://192.168.1.81:3000/api/fence/fence",
         requestData
       );
 
